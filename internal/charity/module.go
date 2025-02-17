1ac9f47e605986cdddf0c98844050b4220b67be0
gravitino <ngobinh0312@gmail.com>
2025-02-17 14:44:30 +0700

    add constructor to charity module

--- internal/charity/module.go
+++ internal/charity/module.go
@@ -2,11 +2,15 @@ package charity
 
 import (
 	"github.com/charitan-go/profile-server/internal/charity/handler"
+	"github.com/charitan-go/profile-server/internal/charity/repository"
+	"github.com/charitan-go/profile-server/internal/charity/service"
 	"go.uber.org/fx"
 )
 
 var CharityModule = fx.Module("charity",
 	fx.Provide(
 		handler.NewCharityHandler,
+		service.NewCharityService,
+		repository.NewCharityRepository,
 	),
 )
